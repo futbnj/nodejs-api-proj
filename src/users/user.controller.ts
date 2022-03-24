375ef65dde224f1c658b47fe82c83f0e9f362c21
futbnj <d.kolisnichenko.sas@gmail.com>
2022-03-24 13:04:47 +0300

    мфдшвфеÑvalidation added

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -10,6 +10,7 @@ import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
 import {UserService} from "./users.service";
 import {IUserService} from "./users.service.interface";
+import {ValidateMiddleware} from "../common/validate.middleware";
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -20,7 +21,12 @@ export class UserController extends BaseController implements IUserController {
 	) {
 		super(loggerService);
 		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)]
+			},
 			{ path: '/login', method: 'post', func: this.login },
 		]);
 	}
